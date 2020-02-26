66834a5a89196a3c338f0a8c80d3fcf3e0a94ef9
Timofey Kotlovoy <timofey1988@inbox.ru>
2020-02-26 23:03:37 +0300

    lesson 14

--- src/project/js/script.js
+++ src/project/js/script.js
@@ -83,15 +83,71 @@ window.addEventListener('DOMContentLoaded', () => {
   container.addEventListener('click', (event) => {
     if (event.target.classList.contains('description-btn') || event.target.classList.contains('more')) {
       overlay.style.display = 'block';
-      this.classList.add('more-splash');
+      container.classList.add('more-splash');
       document.body.style.overflow = 'hidden';
     }
   });
 
   close.addEventListener('click', () => {
     overlay.style.display = 'none';
-    this.classList.remove('more-splash');
+    container.classList.remove('more-splash');
     document.body.style.overflow = '';
   });
+
+  // Form
+
+  let message = {
+	  loading: 'Загрузка...',
+	  success: 'Спасибо! Скоро мы с вами свяжемся!',
+	  failure: 'Что-то пошло не так'
+  };
+
+  let form = document.querySelector('.main-form'),
+  	formBottom = document.getElementById('form'),
+		input = document.getElementsByTagName('input'),
+		statusMessage = document.createElement('div');
+		statusMessage.classList.add('status');
+
+	function sendForm (elem) {
+		elem.addEventListener('submit', function(event) {
+			event.preventDefault();
+			elem.appendChild(statusMessage);
+			let formData = new FormData(elem);
+
+			function postData(data) {
+				return new Promise (function(resolve,reject){
+					let request = new XMLHttpRequest();
+					request.open('POST', 'server.php');
+					request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+					request.addEventListener('readystatechange', function(){
+						if (request.readyState < 4) {
+							resolve();
+						} else if (request.readyState === 4 && request.status == 200) {
+							resolve();
+						} else {
+							reject();
+						}
+					});
+
+					request.send(data);
+				});
+			};
+
+			function clearInput () {
+				for (let i = 0; i < input.length; i++) {
+					input[i].value = '';
+				}
+			};
+
+			postData(formData)
+												.then(() => statusMessage.innerHTML = message.loading)
+												.then(() => statusMessage.innerHTML = message.success)
+												.catch(() => statusMessage.innerHTML = message.failure);
+												clearInput();
+		});
+	};
+	sendForm(form);
+	sendForm(formBottom);
 
 });
